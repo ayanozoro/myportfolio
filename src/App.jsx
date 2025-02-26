531151a272f2c3a0398907ba60e6369f57a7a1e2
“ayanozoro” <mohituniyal1234567@gmail.com>
2025-02-26 15:18:03 +0530

    Renamed navbar.jsx to Navbar.jsx to resolve import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbars/Navbar.jsx';
 import Home from './Components/Homes/Home.jsx';
 import Skills from './Components/Skill/Skills.jsx';
 import About from './Components/Abouts/About.jsx'; 
 import Project from './Components/Proj/Project.jsx';
 import Contact from './Components/Contacts/Contact.jsx';
-import Navbar from './Components/Navbars/Navbar.jsx';
 
 function App() {
   return (
