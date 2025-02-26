02240260a077f553634ed5da931c7af33c6e6411
“ayanozoro” <mohituniyal1234567@gmail.com>
2025-02-26 15:05:31 +0530

    Update components with latest chs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbars/Navbar.jsx';
-import Home from './components/Home/Home';
+import Home from './Components/Homes/Home.jsx';
 import Skills from './Components/Skill/Skills.jsx';
 import About from './Components/Abouts/About.jsx'; 
 import Project from './Components/Proj/Project.jsx';
