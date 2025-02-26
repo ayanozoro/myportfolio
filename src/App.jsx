7125317d6bf042ef5be02a7de36e416427e2e559
“ayanozoro” <mohituniyal1234567@gmail.com>
2025-02-26 15:12:08 +0530

    Renamed navbar.jsx to Navbar.jsx to resolve import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbars/Navbar.jsx';
 import Home from './Components/Homes/Home.jsx';
 import Skills from './Components/Skill/Skills.jsx';
 import About from './Components/Abouts/About.jsx'; 
 import Project from './Components/Proj/Project.jsx';
 import Contact from './Components/Contacts/Contact.jsx';
+import Navbar from './Components/Navbars/Navbar.jsx';
 
 function App() {
   return (
     <div className="App w-full flex flex-col overflow-x-hidden">
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Project />
-      <Contact />
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Contact/>
     </div>
   );
 }
